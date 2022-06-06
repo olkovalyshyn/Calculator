c2fb82b244c3c207ab0db2d0c0d84327f4e80e39
olkovalyshyn <kovalyshyn@gmail.com>
2022-06-06 22:45:22 +0300

    Add handle changes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,30 @@ import Buttons from './component/Buttons/Buttons';
 import Display from './component/Display/Display';
 
 function App() {
-  const [firstItem, setFirstItem] = useState();
-  const [secondItem, setSecondItem] = useState();
-  const hundleSubmit = event => {
-    console.log('CLICK');
-    console.log(event.currentTarget.value);
-    setFirstItem(event.currentTarget.value);
+  const [result, setResult] = useState('');
+  // const [secondItem, setSecondItem] = useState();
+  const handleSubmit = event => {
+    // console.log('CLICK');
+    // console.log(event.currentTarget.value);
+    setResult(result.concat(event.currentTarget.value));
+  };
+
+  const handleClear = () => {
+    setResult('');
+  };
+
+  const handleBackSpace = () => {
+    setResult(result.slice(0, -1));
   };
 
   return (
     <div>
-      <Display inputValue={firstItem} />
-      <Buttons onClickInput={hundleSubmit} />
+      <Display inputValue={result} />
+      <Buttons
+        onClickInput={handleSubmit}
+        onClear={handleClear}
+        onBackSpace={handleBackSpace}
+      />
     </div>
   );
 }
--- src/component/Buttons/Buttons.jsx
+++ src/component/Buttons/Buttons.jsx
@@ -1,25 +1,64 @@
-import React, {useState} from "react";
+// import React, {useState} from "react";
 
-function Buttons({onClickInput}) {
-  const [btn1, setBtn1 ]= useState()
-//   const { secondItem, setSecondItem } = useState()
-//   const hundleSubmit = e => {
-//     console.log(e.currentTarget.value)
-//     setBtn1(e.currentTarget.value)
-  
-// }
+function Buttons({ onClickInput, onClear, onBackSpace }) {
+  // const [btn1, setBtn1 ]= useState()
+  //   const { secondItem, setSecondItem } = useState()
+  //   const hundleSubmit = e => {
+  //     console.log(e.currentTarget.value)
+  //     setBtn1(e.currentTarget.value)
+
+  // }
 
   return (
     <>
-      <button value="1" onClick={onClickInput}>1</button>
-      <button value="2" onClick={onClickInput}>2</button>
-      <button value="3" onClick={onClickInput}>3</button>
-      <button value="4" onClick={onClickInput}>4</button>
-      <button value="5" onClick={onClickInput}>5</button>
-      <button value="6" onClick={onClickInput}>6</button>
-      <button value="7" onClick={onClickInput}>7</button>
-      <button value="8" onClick={onClickInput}>8</button>
-      <button value="9" onClick={onClickInput}>9</button>
+            <button  onClick={onClear}>
+        C
+      </button>
+
+      <button  onClick={onBackSpace}>
+        back space
+      </button>
+
+
+      <button value="1" onClick={onClickInput}>
+        1
+      </button>
+      <button value="2" onClick={onClickInput}>
+        2
+      </button>
+      <button value="3" onClick={onClickInput}>
+        3
+      </button>
+      <button value="4" onClick={onClickInput}>
+        4
+      </button>
+      <button value="5" onClick={onClickInput}>
+        5
+      </button>
+      <button value="6" onClick={onClickInput}>
+        6
+      </button>
+      <button value="7" onClick={onClickInput}>
+        7
+      </button>
+      <button value="8" onClick={onClickInput}>
+        8
+      </button>
+      <button value="9" onClick={onClickInput}>
+        9
+      </button>
+      <button value="+" onClick={onClickInput}>
+        +
+      </button>
+      <button value="-" onClick={onClickInput}>
+        -
+      </button>
+      <button value="*" onClick={onClickInput}>
+        *
+      </button>
+      <button value="/" onClick={onClickInput}>
+        /
+      </button>
     </>
   );
 }
